9f5fa440829bd7c86a868737c49c1f48414b287a
MarlosEmanuel <marlosemanuel28@gmail.com>
2025-05-14 20:26:19 -0300

    4.0

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res) => {
-    res.send('Olá autenticação 3.0 !');
+    res.send('Olá autenticação 4.0 !');
 });
 
 app.listen(PORT, () =>{
