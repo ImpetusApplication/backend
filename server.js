7453cf8f94b26e25ac33821126d3bb6c2eb09562
MarlosEmanuel <marlosemanuel28@gmail.com>
2025-05-15 01:19:30 -0300

    criando o esquema de testes com Jest

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const express = require('express');
-
-const app = express();
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req,res) => {
-    res.send('Olá mundo !');
-});
-
-app.listen(PORT, () =>{
-    console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
