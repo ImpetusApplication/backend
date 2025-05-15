81e4719c98bb14d29a4eb6db58fbccf749cb1cd0
MarlosEmanuel <marlosemanuel28@gmail.com>
2025-05-15 14:59:21 -0300

    build: configura o server para as novas rotas e instala novas dependencias

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
+app.use(express.json());
+
+app.use('/users', userRoutes);
 
 app.get('/', (req,res) => {
     res.send('Olá mundo !');
