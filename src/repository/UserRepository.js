c25ec8e4a56689efc06681b3bc78a3645547e3a0
MarlosEmanuel <marlosemanuel28@gmail.com>
2025-05-15 14:57:51 -0300

    feat: cria o UserRepository para se comunicar com o banco de dados

---
+++ src/repository/UserRepository.js
@@ -0,0 +1,40 @@
+const User = require('../model/User');
+
+class UserRepository {
+
+    // Cria um usuario
+    async create(userData){
+
+        try{
+            const user = await User.create(userData);
+            return user;
+        }
+        catch(error){
+            throw new Error('Falha ao salvar usuario: ' + error);
+        }
+    }
+
+    // Busca todos os usuarios no banco de dados
+    async findAll(){
+        const users = await User.findAll();
+        return users;
+    }
+
+    // Busca um usuario por ID
+    async findById(id){
+        const user = await User.findById(id);
+        return user;
+    }
+
+    // Delete um usuario por ID
+    async delete(id){
+        const user = await this.findById(id);
+        if(!user){
+            return false;
+        }
+        await user.destroy();
+        return true;
+    }
+}
+
+module.exports = new UserRepository();
