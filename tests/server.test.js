23f6d2638a58b9237a51cb9a64e66ea2ced292cc
MarlosEmanuel <marlosemanuel28@gmail.com>
2025-05-15 01:31:39 -0300

    organizando repositorio

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const app = require('../../app');
+const app = require('../src/app');
 
 describe('Testando GET /', ()=>{
     it('deve retornar Olá mundo !', async () => {
