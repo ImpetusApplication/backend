fbc10eada1f54f00ffb2ea30e0f29d829d644fc3
MarlosEmanuel <marlosemanuel28@gmail.com>
2025-05-15 19:12:20 -0300

    feat: cria a documentação da API de cadastro de usuario

--- src/app.js
+++ src/app.js
@@ -1,15 +1,71 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
-const autenticador = require('./middleware/authMiddleware'); // importe o middleware
+const autenticador = require('./middleware/authMiddleware');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 app.use(express.json());
 
-// Protege a rota '/' com o middleware de autenticação
-app.get('/', autenticador, (req, res) => {
-  res.send('Olá mundo !');
-});
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'API Impetus',
+    version: '1.0.0',
+    description: 'Documentação da API do projeto Impetus',
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+    },
+  ],
+  components: {
+    schemas: {
+      User: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'integer',
+            example: 1,
+          },
+          name: {
+            type: 'string',
+            example: 'João Silva',
+          },
+          email: {
+            type: 'string',
+            format: 'email',
+            example: 'joao.silva@example.com',
+          },
+          birthdate: {
+            type: 'string',
+            format: 'date',
+            example: '1990-05-15',
+          },
+          createdAt: {
+            type: 'string',
+            format: 'date-time',
+            example: '2023-05-01T12:00:00Z',
+          },
+          updatedAt: {
+            type: 'string',
+            format: 'date-time',
+            example: '2023-05-02T15:30:00Z',
+          },
+        },
+      },
+    },
+  },
+};
 
+const options = {
+  swaggerDefinition,
+  apis: ['./src/routes/*.js']
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/users', userRoutes);
 
 module.exports = app;
